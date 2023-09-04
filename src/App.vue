0add0b8dbee2366be2dcf3967c703029b66d5387
AmaliaRibeiro <amalita9421@gmail.com>
2023-09-03 23:22:14 -0300

    folders and first changes

---
+++ src/App.vue
@@ -0,0 +1,91 @@
+<script setup>
+import {ref, reactive, onMounted, watch } from 'vue'
+import {db} from './data/zapatos'
+import Zapato from './components/Zapato.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+
+const zapatos = ref([])
+const carrito = ref([])
+const zapato = ref ([])
+
+watch(
+  carrito, () =>{
+    guardarLocalStorage();
+  }, {
+    deep: true // si es una estructura muy grande no sirve bien.
+  }
+)
+
+onMounted(() => {
+  zapatos.value = db
+  zapato.value = db[2]
+
+  const carritoStorage = localStorage.getItem('carrito')
+  if(carritoStorage) {
+    carrito.value = JSON.parse(carritoStorage)
+  }
+})
+
+const guardarLocalStorage = () => {
+  localStorage.setItem('carrito', JSON.stringify(carrito.value))
+}
+ const agregarCarrito = (zapato) => {
+  const existeCarrito = carrito.value.findIndex(producto => producto.id === zapato.id)
+  if (existeCarrito >= 0) {
+    carrito.value[existeCarrito] //si quiero agregar mas de un zapato del mismo modelo se agrega .cantidad++
+  } else {
+    zapato.cantidad = 1;
+    carrito.value.push(zapato);
+  }
+ }
+
+ const decrementarCantidad = (id) => {
+  const index = carrito.value.findIndex(producto => producto.id === id)
+  if(carrito.value[index].cantidad <= 1) return //validacion para que no baje del 1 la cantidad
+  carrito.value[index].cantidad--
+ }
+
+ const incrementarCantidad = (id) => {
+  const index = carrito.value.findIndex(producto => producto.id === id)
+  if(carrito.value[index].cantidad >= 1) return //para que no sume mas de 1 en cantidad.
+  carrito.value[index].cantidad++
+ }
+
+ const eliminarProducto = (id) => {
+    carrito.value = carrito.value.filter(producto => producto.id !== id)
+ }
+
+ const vaciarCarrito = () => {
+  carrito.value = []
+ } 
+
+</script>
+
+<template>
+<Header 
+  :carrito="carrito"
+  :zapato="zapato"
+  @incrementar-cantidad ="incrementarCantidad"
+  @decrementar-cantidad ="decrementarCantidad"
+  @agregar-carrito="agregarCarrito"
+  @eliminar-producto="eliminarProducto"
+  @vaciar-carrito="vaciarCarrito"
+/> 
+   
+    <main class="container-xl mt-5">
+        <h2 class="text-center">Nuestra Colección</h2>
+
+        <div class="row mt-5">
+           <Zapato
+           v-for="zapato in zapatos"
+           v-bind:zapato="zapato"
+           @agregar-carrito="agregarCarrito"
+          />
+        </div>
+    </main>
+
+<Footer/>
+</template>
+
+
---
+++ src/components/Zapato.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+const props = defineProps({
+    zapato: {
+     type: Object,
+     required: true
+    }
+})
+
+defineEmits(['agregar-carrito'])
+</script>
+
+<template>
+<div class="col-md-6 col-lg-4 my-4 row align-items-center">
+    <div class="col-4">
+        <img class="img-fluid" 
+        v-bind:src="'/img/' + zapato.imagen + '.png'" 
+        :alt="'imagen guitarra' + zapato.nombre">
+    </div>
+    <div class="col-8">
+        <h3 class="text-black fs-4 fw-bold text-uppercase">{{ zapato.nombre}}</h3>
+        <p>{{ numero }}</p>
+        <p>{{ zapato.descripcion }}</p>
+        <p class="fw-black text-primary fs-3">${{ zapato.precio }}</p>
+        <button 
+            type="button"
+            class="btn btn-dark w-100 "
+            v-on:click="$emit('agregar-carrito', zapato)"
+        >Agregar al Carrito</button>
+    </div>
+</div><!-- FIN GUITARRA -->
+</template>
