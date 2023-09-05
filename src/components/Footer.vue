db3e95de60a83027a5cbc9390d4e0a57bb1ede76
AmaliaRibeiro <amalita9421@gmail.com>
2023-09-04 23:50:35 -0300

    feat: change footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
 
 <footer class="bg-dark mt-5 py-5">
         <div class="container-xl">
-            <p class="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+            <p class="text-white text-center fs-4 mt-4 m-md-0">Amma tienda online de calzados para mujeres</p>
         </div>
     </footer>
     
