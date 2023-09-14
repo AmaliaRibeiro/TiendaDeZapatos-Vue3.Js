f9771974fa109a7752bed0b92c1c10782247ba24
AmaliaRibeiro <amalita9421@gmail.com>
2023-09-14 01:07:24 -0300

    Feat: icono whatsap

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,8 +2,29 @@
 
 <footer class="bg-dark mt-5 py-5">
         <div class="container-xl">
-            <p class="text-white text-center fs-4 mt-4 m-md-0">Amma tienda online de calzados para mujeres</p>
+            <p class="text-white text-center fs-4 mt-4 m-md-0">Haz click en el ícono para enviar mensaje de whatsap y confirmar tu pedido</p><hr>
+            <a href="https://wa.me/543764210156" class="whatsapp" target="_blank"><i class="fa fa-whatsapp whatsapp-icon"></i></a>
         </div>
     </footer>
     
 </template>
+<style scope>
+
+.whatsapp {
+  position:fixed;
+  width:60px;
+  height:60px;
+  bottom:40px;
+  right:130px;
+  background-color:#25d366;
+  color:#FFF;
+  border-radius:50px;
+  text-align:center;
+  font-size:30px;
+  z-index:100;
+}
+
+.whatsapp-icon {
+  margin-top:13px;
+}
+</style>
